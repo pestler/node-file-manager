d80eb4dd248310254c3371604c90598796470f15
Aleh Kuis <pestleralex@gmail.com>
2024-10-15 18:12:30 +0300

    refactoring: refactoring

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 import process from 'node:process';
 import readline from 'readline';
 import { switchAPI } from '../src/commands/switch-api.js';
+import os from 'os';
 
 let username = 'user'
 
@@ -9,18 +10,24 @@ const startCLI = async () => {
     const output = process.stdout;
     const rl = readline.createInterface({ input, output });
     
-    process.stdout.write(`Welcome to the File Manager, ${username}!\n`);    
-        
+    console.log(`Welcome to the File Manager, ${username}!\n`);    
+    console.log(`You are currently in ${os.homedir()};                       }\n`);  
+
+    rl.on('SIGINT', () => {
+        console.log(`Thank you for using File Manager, ${username}!`);  
+        process.exit(0);
+        rl.close();          
+    });
+     
     rl.on('line', async (input) => {          
         const [command, ...params] = input.toString().trim().split(' ');
         if (input === '.exit') {
-            process.stdout.write(`Thank you for using File Manager, ${username}!`);  
+            console.log(`Thank you for using File Manager, ${username}!`);  
             rl.close();          
         } else {                                    
             await switchAPI(command, params);            
         }
     })                    
 };
-
 export default startCLI;
 
